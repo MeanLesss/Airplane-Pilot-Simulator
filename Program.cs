f90eb6cbe79d38012685468800a7997e559c5e7a
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2021-11-25 13:41:39 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Airplane_pilot_simulator_KANG_sokvimean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+
+
+            ReadKey();
+        }
+    }
+}
